6cd092fd47569708b563956d20a8b1f31df2a90c
Warano02 <carineteoi@gmail.com>
2025-05-15 11:35:06 +0100

    home pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import NavBar from './components/NavBar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller")
   return (
     <div>
       {isSellerPath ? null : <NavBar />}
-      <Toaster/>
+      <Toaster />
       <div className={`${isSellerPath ? "" : " px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   )
 }
