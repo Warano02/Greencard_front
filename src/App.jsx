294c2c93a49006ac30386588e86d8f271d07e02d
Warano02 <carineteoi@gmail.com>
2025-05-16 14:31:36 +0100

    add product

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
 import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller")
   const { showUserLogin, isSeller } = useAppContext()
@@ -32,7 +35,9 @@ function App() {
           <Route path="/products/:category" element={<ProductCategory />} />
           <Route path="/products/:category/:id" element={<ProductsDetails />} />
           <Route path='/seller' element={isSeller ? <SellerLayout /> : <SellerLogin />}>
-
+            <Route index element={isSeller ? <AddProduct /> : null} />
+            <Route path='product-list' element={<ProductList />} />
+            <Route path='orders' element={<Orders />} />
           </Route>
         </Routes>
       </div>
